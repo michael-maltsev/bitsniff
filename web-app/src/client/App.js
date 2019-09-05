66c213cf309802bdb7564f1c9f1db9d93ad7cbb3
m417z <mmm.maltsev@gmail.com>
2019-09-05 15:18:25 +0300

    Added an initial version of the frontend

---
+++ web-app/src/client/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './app.css';
+import logoImage from './assets/logo.png';
+import Dropzone from 'react-dropzone';
+
+export default class App extends Component {
+  state = { username: null };
+
+  constructor(props) {
+    super(props);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  onDrop(acceptedFiles) {
+    if (acceptedFiles.length === 0) {
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log('file reading was aborted');
+    reader.onerror = () => console.log('file reading has failed');
+    reader.onload = () => {
+      // Do whatever you want with the file contents
+      const str = reader.result;
+      console.log(str);
+    };
+
+    const file = acceptedFiles[0];
+    reader.readAsText(file);
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <img src={logoImage} className='logoImage' alt='logo' />
+          <div>BitSniff</div>
+        </header>
+        <section className='mainSection'>
+          <Dropzone onDrop={this.onDrop}>
+            {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject }) => {
+              let divClassName = 'dropzoneBaseStyle';
+              if (isDragActive) {
+                divClassName += ' dropzoneActiveStyle';
+              }
+              if (isDragAccept) {
+                divClassName += ' dropzoneAcceptStyle';
+              }
+              if (isDragReject) {
+                divClassName += ' dropzoneRejectStyle';
+              }
+              return (
+                <section>
+                  <div {...getRootProps({ className: divClassName })}>
+                    <input {...getInputProps()} />
+                    <p>Drop the network log file to be analyzed</p>
+                  </div>
+                </section>
+              );
+            }}
+          </Dropzone>
+        </section>
+        <footer>
+          &copy; The BitSniff team
+        </footer>
+      </>
+    );
+  }
+}
