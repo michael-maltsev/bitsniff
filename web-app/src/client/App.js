1705ff32127d72716f954fff0be2730d7516ca1f
m417z <mmm.maltsev@gmail.com>
2019-09-05 22:54:04 +0300

    Calc percentage with ztable, graph threshold, normalize block data based on traffic

--- web-app/src/client/App.js
+++ web-app/src/client/App.js
@@ -3,6 +3,7 @@ import './app.css';
 import logoImage from './assets/logo.png';
 import Dropzone from 'react-dropzone';
 import BarLoader from 'react-spinners/BarLoader';
+import ztable from 'ztable';
 import AnalysisChart from './components/AnalysisChart'
 
 export default class App extends Component {
@@ -15,6 +16,7 @@ export default class App extends Component {
     super(props);
     this.onDrop = this.onDrop.bind(this);
     this.onHeaderClick = this.onHeaderClick.bind(this);
+    this.calcResultPercentage = this.calcResultPercentage.bind(this);
   }
 
   onDrop(acceptedFiles) {
@@ -53,6 +55,12 @@ export default class App extends Component {
     this.setState({ analyzerData: null });
   }
 
+  calcResultPercentage(result) {
+    const percentage = Number((ztable(result) * 100).toFixed(1));
+    const prefix = percentage === 100 ? 'nearly ' : '';
+    return `${prefix}${percentage}%`;
+  }
+
   render() {
     const { analyzerDataLoading, analyzerData } = this.state;
     return (
@@ -73,7 +81,7 @@ export default class App extends Component {
               <>
                 <div className='verdictText'>
                   {'The file represents Bitcoin activity with '}
-                  <span className='verdictTextNumber'>{Number(analyzerData.result.toFixed(1))}%</span> probability
+                  <span className='verdictTextNumber'>{this.calcResultPercentage(analyzerData.result)}</span> probability
                 </div>
                 <AnalysisChart data={analyzerData} />
               </>
