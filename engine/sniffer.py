8fa2a832ce28189f85bb39fda0d1906e62e58690
Tommy <ku@campus.technion.ac.il>
2019-09-05 14:13:42 +0300

    sniffer - fakes generation

--- engine/sniffer.py
+++ engine/sniffer.py
@@ -9,13 +9,19 @@ def similarity(a, b):
     return np.trapz(a * b)
 
 # Generate fake block time-series of given length
-def generateFake(length):
-    return np.zeros(length)
+def generateFake(real):
+    fake = np.zeros(len(real))
+    size = np.sum(real) / np.count_nonzero(real)
+    freq = np.count_nonzero(real) / len(real)
+    for i in range(len(fake)):
+        if np.random.random() < freq:
+            fake[i] = size
+    return fake
 
 # Create bell shapes around blocks
 def shapePredict(data):
     return data
-    
+
 # Calculate the probability of detection (in units of std)
 def detect(traffic, blocks):
     if len(traffic) != len(block):
@@ -25,7 +31,7 @@ def detect(traffic, blocks):
 
     fake_results = np.zeros(k_fakes_num)
     for i in range(k_fakes_num):
-        fake = generateFake(length)
+        fake = generateFake(blocks)
         fake_results[i] = similarity(traffic, shapePredict(fake))
 
     real_result = similarity(traffic, shapePredict(blocks))
