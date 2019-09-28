f6bc0625f42e377879cfddc114436e5c3cf7e420
Tommy <ku@campus.technion.ac.il>
2019-09-28 05:24:58 +0300

    moved changes to analyze.py

--- engine/analyze.py
+++ engine/analyze.py
@@ -8,6 +8,7 @@ import sys
 import pickle
 import json
 import sniffer
+from scapy.all import *
 
 # Parse .pcap file into python array and start timestamp
 def parsePcap(packets):
@@ -34,46 +35,13 @@ def parsePcap(packets):
 
     return (ts_arr, start)
 
-
-# Parse log into python array and start timestamp
-def parseLog():
-    lines = sys.stdin.readlines()
-    ts_dict = {}
-
-    for line in lines:
-        arr = line.split()
-        time = [int(t) for t in arr[0].split('.')[0].split(':')]
-        try:
-            size = int(arr[-1])
-        except:
-            size = 0
-
-        if len(sys.argv) > 2:
-            day = int(sys.argv[2])
-
-        dt = datetime(2019, 9, day, time[0], time[1], time[2])
-        key = int(datetime.timestamp(dt))
-        if key in ts_dict:
-            ts_dict[key] += size
-        else:
-            ts_dict[key] = size
-
-    start = sorted(ts_dict.keys())[0]
-    end = sorted(ts_dict.keys())[-1]
-
-    ts_arr = []
-    for t in range(start, end + 1):
-        if t in ts_dict:
-            ts_arr += [ts_dict[t]]
-        else:
-            ts_arr += [0]
-
-    return (ts_arr, start)
-
 # Get the blockchain data (BTC)
-def getBlocksDict():
+def getBlocksDict(timestamp, length):
+    start = str(datetime.fromtimestamp(timestamp)).split()[0]
+    end = str(datetime.fromtimestamp(timestamp + length)).split()[0]
+
     request = 'https://api.blockchair.com/bitcoin/'
-    request += 'blocks?q=time(2019-09-04..2019-09-06)&limit=100&offset='
+    request += 'blocks?q=time(' + start + '..' + end + ')&limit=100&offset='
 
     limit = 100
     offset = 0
@@ -116,18 +84,10 @@ def getBlocksArray(start, length, b_dict):
 
     return b_arr
 
-traffic, start = parseLog()
+packets = rdpcap(sys.stdin.buffer)
+traffic, start = parsePcap(packets)
 
-pkl_path = './blocks_dict.pickle'
-
-if os.path.isfile(pkl_path):
-    with open(pkl_path, 'rb') as pkl_file:
-        blocks_dict = pickle.load(pkl_file)
-else:
-    blocks_dict = getBlocksDict()
-    with open(pkl_path, 'wb') as pkl_file:
-        pickle.dump(blocks_dict, pkl_file)
-
+blocks_dict = getBlocksDict(start, len(traffic))
 blocks_data = getBlocksArray(start, len(traffic), blocks_dict)
 blocks_web = list(sniffer.bellShape(blocks_data))
 blocks_local = sniffer.shapePredict(blocks_data)
